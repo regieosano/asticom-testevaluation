cd17eb1f348093d2eaae46c0aaa53c53e05b551c
regieosano <osanoreginald@gmail.com>
2021-08-23 09:37:51 +0800

    Added dummy DB

--- src/claudia-apis/controllers/customer.js
+++ src/claudia-apis/controllers/customer.js
@@ -1,6 +1,10 @@
+// Function Controllers
+
+import customers from '../../db/db';
+
 exports.getCustomers = (req, res) => {
     try {
-        res.status(200).send({ data: {} });
+        res.status(200).send({ data: customers });
     } catch (e) {
         console.log(error);
     }
---
+++ src/db/db.js
@@ -0,0 +1,28 @@
+const customers = [{
+        id: 'c1',
+        name: 'Karl James',
+        address: 'Project 8 QC'
+    },
+    {
+        id: 'c2',
+        name: 'Mike Cruz',
+        address: 'Fairview QC'
+    },
+    {
+        id: 'c3',
+        name: 'Anna Reyes',
+        address: 'Cubao QC'
+    },
+    {
+        id: 'c4',
+        name: 'James Monroe',
+        address: 'Taft, Manila'
+    },
+    {
+        id: 'c5',
+        name: 'John De la Cruz',
+        address: 'Pasig City'
+    }
+];
+
+module.exports = customers;
