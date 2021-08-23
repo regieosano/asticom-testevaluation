b68a6b53ada612c6e33b66a9d36fc243f9fffc0b
regieosano <osanoreginald@gmail.com>
2021-08-23 11:29:14 +0800

    Corrected controller for the DELETE route

--- src/claudia-apis/app.js
+++ src/claudia-apis/app.js
@@ -6,4 +6,4 @@ apiClaudia.get('/customers', customerController.getCustomers);
 apiClaudia.post('/customers', customerController.postCustomers);
 apiClaudia.get('/customers/:id', customerController.getCustomer);
 apiClaudia.put('/customers/:id', customerController.updateCustomer);
-apiClaudia.delete('/customers/:id', customerController.updateCustomer);
+apiClaudia.delete('/customers/:id', customerController.deleteCustomer);
